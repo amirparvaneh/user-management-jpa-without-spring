61b7574ca7da3c7a142f5e446d1940977e854fd0
amirparvaneh <amirparvaneh138@gmail.com>
2022-11-24 00:12:57 +0330

    v1

--- usermanagement/src/main/java/com/digipay/usermanagement/App.java
+++ usermanagement/src/main/java/com/digipay/usermanagement/App.java
@@ -13,19 +13,28 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        //Logger logger ;
-//        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
-//        EntityManager entityManager = entityManagerFactory.createEntityManager();
-//        EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
-//        entityManager.getTransaction().begin();
-        //we are saving permission : save permission
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter an operation ? ");
-        System.out.println(" 1 = User Operation \n 2 = Role Operation \n  3 = Permission Operation ");
+        System.out.println(" 1 = User Operation \n 2 = Role Operation \n 3 = Permission Operation");
         Integer inputOperation = scanner.nextInt();
         switch (inputOperation) {
             case 1: {
+                System.out.println(" 1 = createUser \n 2 = deleteUser \n 3 = updateUser \n 4 = listAllUser ");
+                Integer userChosen = scanner.nextInt();
+                switch (userChosen) {
+                    case 1: {
 
+                    }
+
+                    case 2: {
+
+                    }
+                    case 3: {
+                    }
+                    case 4: {
+                    }
+                }
             }
             case 2: {
                 System.out.println(" 1 = Role create \n 2 = deleteRole \n  3 = updateRole \n 4 = listAllRole ");
@@ -57,110 +66,39 @@ public class App {
                         role.setTitle(roleTile);
                         roleService.save(role);
                     }
+                    case 2: {
+
+                    }
                     case 3: {
-                        System.out.println(" 1 = permission create \n 2 = deletePermission \n  3 = updatePermission \n 4 = listAllPermission ");
-                        PermissionServiceImpl permissionService = new PermissionServiceImpl();
-                        Integer permissionOperation = scanner.nextInt();
-                        switch (permissionOperation) {
-                            case 1: {
-                                System.out.println("Enter Permission Title :");
-                                String title = scanner.next();
-                                Permission permission = new Permission();
-                                permission.setTitle(title);
-                                permissionService.save(permission);
-                            }
-                            case 2: {
-
-                            }
-                            case 3: {
-
-                            }
-                            case 4: {
-
-                            }
-                        }
+                    }
+                    case 4: {
                     }
                 }
+            }
 
-                //todo : get permission info from console -> title;
-//        System.out.println("Enter a permission title : ");
-//        String title = scanner.nextLine();
-//        PermissionServiceImpl permissionCrud = new PermissionServiceImpl();
-//        Permission permission = new Permission();
-//        permission.setTitle(title);
-//        permissionCrud.save(permission);
-//        //todo : get Role info from console;
-//        System.out.println("Enter role title : ");
-//        String roleTitle = scanner.nextLine();
-//        Role role = new Role();
-//        role.setTitle(roleTitle);
-////        role.setPermissions();
-//        RoleCrudImpl roleCrud = new RoleCrudImpl();
-//        roleCrud.save(role);
-//        //todo : get user info
-//        System.out.println("Enter your name : ");
-//        String name = scanner.nextLine();
-//        System.out.println("Enter your national code : ");
-//        Long nationalCode = scanner.nextLong();
-//        User user1 = new User();
-//        user1.setName(name);
-//        user1.setNationalID(nationalCode);
-//        UserCrudImpl userCrud = new UserCrudImpl();
-//        userCrud.save(user1);
+            case 3: {
+                System.out.println(" 1 = permission create \n 2 = deletePermission \n  3 = updatePermission \n 4 = listAllPermission ");
+                PermissionServiceImpl permissionService = new PermissionServiceImpl();
+                Integer permissionOperation = scanner.nextInt();
+                switch (permissionOperation) {
+                    case 1: {
+                        System.out.println("Enter Permission Title :");
+                        String title = scanner.next();
+                        Permission permission = new Permission();
+                        permission.setTitle(title);
+                        permissionService.save(permission);
+                    }
+                    case 2: {
+
+                    }
+                    case 3: {
+
+                    }
+                    case 4: {
+
+                    }
+                }
             }
         }
-
     }
 }
-//package com.digipay;
-//
-//        import jakarta.persistence.EntityManagerFactory;
-//        import jakarta.persistence.Persistence;
-//
-//public class JPAUtil {
-//    private static final String PERSISTENCE_UNIT_NAME = "TestPersistence";
-//    private static EntityManagerFactory factory;
-//
-//    public static EntityManagerFactory getEntityManagerFactory() {
-//        if (factory == null) {
-//            factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-//        }
-//        return factory;
-//    }
-//
-//    public static void shutdown() {
-//        if (factory != null) {
-//            factory.close();
-//        }
-//    }
-//}
-
-//package com.digipay;
-//
-//        import com.digipay.model.User;
-//        import jakarta.persistence.EntityManager;
-//        import org.hibernate.Session;
-//        import org.slf4j.Logger;
-//        import org.slf4j.LoggerFactory;
-//
-///**
-// * Hello world!
-// */
-//public class JpaMain {
-//    public static void main(String[] args) {
-//        EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
-//        entityManager.getTransaction().begin();
-//
-//        User user = new User();
-//        user.setFirstName("Pankaj");
-//        user.setLastName("CEOi");
-//        user.setUsername("888pan");
-//        user.setPassword("888");
-//        user.setAge(19);
-//        entityManager.persist(user);
-//        entityManager.getTransaction().commit();
-//        entityManager.close();
-//
-//        JPAUtil.shutdown();
-//    }
-//}
